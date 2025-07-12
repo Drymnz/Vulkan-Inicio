69f7d058aed1965dd3e1c1843cd40b7816bb08e2
Benjamìn de Jesùs <mcdreck276@gmail.com>
2025-07-12 11:51:09 -0600

    implementacion del pickPhysicalDevice

--- HelloTriangleApplication.cpp
+++ HelloTriangleApplication.cpp
@@ -1,10 +1,11 @@
 #include "HelloTriangleApplication.h"
 #include <iostream>
+#include <set>
+#include <cstring>
 
 const uint32_t WIDTH = 800;
 const uint32_t HEIGHT = 600;
 
-// Implementación de los métodos (sin definir run() otra vez)
 void HelloTriangleApplication::initWindow() {
     glfwInit();
     glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
@@ -14,9 +15,14 @@ void HelloTriangleApplication::initWindow() {
 
 void HelloTriangleApplication::initVulkan() {
     createInstance();
+    pickPhysicalDevice(); // <--- Aquí llamamos a la función
 }
 
 void HelloTriangleApplication::createInstance() {
+    if (enableValidationLayers && !checkValidationLayerSupport()) {
+        throw std::runtime_error("validation layers requested, but not available!");
+    }
+
     VkApplicationInfo appInfo{};
     appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
     appInfo.pApplicationName = "Hello Triangle";
@@ -33,13 +39,19 @@ void HelloTriangleApplication::createInstance() {
     const char **glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
     createInfo.enabledExtensionCount = glfwExtensionCount;
     createInfo.ppEnabledExtensionNames = glfwExtensions;
-    createInfo.enabledLayerCount = 0;
+
+    if (enableValidationLayers) {
+        createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+        createInfo.ppEnabledLayerNames = validationLayers.data();
+    } else {
+        createInfo.enabledLayerCount = 0;
+    }
 
     if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
         throw std::runtime_error("¡Error al crear la instancia de Vulkan!");
     }
 
-    // Verificar extensiones (opcional)
+    // Mostrar extensiones disponibles
     uint32_t extensionCount = 0;
     vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
     std::vector<VkExtensionProperties> extensions(extensionCount);
@@ -49,6 +61,58 @@ void HelloTriangleApplication::createInstance() {
     for (const auto &extension : extensions) {
         std::cout << "\t" << extension.extensionName << "\n";
     }
+}
+
+void HelloTriangleApplication::pickPhysicalDevice() {
+    uint32_t deviceCount = 0;
+    vkEnumeratePhysicalDevices(instance, &deviceCount, nullptr);
+    if (deviceCount == 0) {
+        throw std::runtime_error("¡No se encontraron GPUs con soporte para Vulkan!");
+    }
+
+    std::vector<VkPhysicalDevice> devices(deviceCount);
+    vkEnumeratePhysicalDevices(instance, &deviceCount, devices.data());
+
+    for (const auto &device : devices) {
+        if (isDeviceSuitable(device)) {
+            physicalDevice = device;
+            break;
+        }
+    }
+
+    if (physicalDevice == VK_NULL_HANDLE) {
+        throw std::runtime_error("¡No se encontró una GPU adecuada!");
+    }
+}
+
+bool HelloTriangleApplication::isDeviceSuitable(VkPhysicalDevice device) {
+    QueueFamilyIndices indices = findQueueFamilies(device);
+    return indices.isComplete();
+}
+
+HelloTriangleApplication::QueueFamilyIndices HelloTriangleApplication::findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+
+    uint32_t queueFamilyCount = 0;
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto &queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+        }
+
+        if (indices.isComplete()) {
+            break;
+        }
+
+        i++;
+    }
+
+    return indices;
 }
 
 void HelloTriangleApplication::mainLoop() {
@@ -63,10 +127,32 @@ void HelloTriangleApplication::cleanup() {
     glfwTerminate();
 }
 
-// Implementación de run() (SOLO UNA VEZ)
 void HelloTriangleApplication::run() {
     initWindow();
     initVulkan();
     mainLoop();
     cleanup();
+}
+
+bool HelloTriangleApplication::checkValidationLayerSupport() {
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- HelloTriangleApplication.h
+++ HelloTriangleApplication.h
@@ -3,21 +3,46 @@
 #include <GLFW/glfw3.h>
 #include <stdexcept>
 #include <vector>
+#include <optional> // necesario para std::optional
 
-class HelloTriangleApplication {
+class HelloTriangleApplication
+{
 public:
-    void run();  // Declaración de run()
-    
+    void run();
+
 private:
-    GLFWwindow* window;
+    GLFWwindow *window;
     VkInstance instance;
     VkPhysicalDevice physicalDevice = VK_NULL_HANDLE;
+
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+    const std::vector<const char *> validationLayers = {
+        "VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+    const bool enableValidationLayers = false;
+#else
+    const bool enableValidationLayers = true;
+#endif
 
     void initWindow();
     void initVulkan();
     void mainLoop();
     void cleanup();
     void createInstance();
-    void pickPhysicalDevice();
-    bool isDeviceSuitable(VkPhysicalDevice device);
+    void pickPhysicalDevice(); // <--- Nuevo método
+    bool isDeviceSuitable(VkPhysicalDevice device); // <--- Nuevo método
+    bool checkValidationLayerSupport();
+
+    struct QueueFamilyIndices {
+        std::optional<uint32_t> graphicsFamily;
+
+        bool isComplete() {
+            return graphicsFamily.has_value();
+        }
+    };
+
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device); // <--- Nuevo
 };
